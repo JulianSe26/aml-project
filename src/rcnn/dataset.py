fixed metric calculation

src/rcnn/dataset.py
@@ -17,7 +17,7 @@ class ChestCocoDetection(CocoDetection):
         # of bounding boxes per default unlike the torchvision transforms
         if self.training:
             self._transforms = A.Compose([
-                A.RandomResizedCrop(height=512, width=512, scale=(.3, 1.0)),
+                A.RandomSizedBBoxSafeCrop(height=512, width=512),
                 A.HorizontalFlip(p=.3),
                 A.ShiftScaleRotate(rotate_limit=20, p=.3, border_mode=0, value=0),
                 A.OneOf([A.Blur(blur_limit=5, p=.25), A.Sharpen(p=.25)], p=.5),
@@ -28,7 +28,7 @@ class ChestCocoDetection(CocoDetection):
             ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']))
         else:
             self._transforms = A.Compose([
-                    A.Resize(512, 512),
+                    A.Resize(1024, 1024),
                     A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                     ToTensorV2()
             ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']))
@@ -58,4 +58,6 @@ class ChestCocoDetection(CocoDetection):
             target['boxes'] = torch.tensor(transformed['bboxes'], dtype=torch.float32)
             target['area'] = torch.tensor((boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]), dtype=torch.float32)
             target['labels'] = torch.tensor(cats, dtype=torch.int64)
+        if not self.training:
+            target['size'] = torch.tensor(img.size, dtype=torch.int16)
         return transformed['image'], target

src/rcnn/train.py
@@ -21,7 +21,7 @@ import pandas as pd
 from rcnn.model import ChestRCNN
 
 '''================Train Configuration========================='''
-number_epochs = 35
+number_epochs = 50
 save_frequency = 2          # in epochs
 test_frequency = 1          # in epochs
 scheduler_frequency = 2     # in epochs
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     parser.add_argument("--ckpt", default=None, type=str)
     parser.add_argument("--train_loss", default=None, type=str)
     parser.add_argument("--eval_stats", default=None, type=str)
-    parser.add_argument("--start_epoch", default=0, type=int)
+    parser.add_argument("--start_epoch", default=1, type=int)
 
     args = parser.parse_args()
 
@@ -70,8 +70,8 @@ if __name__ == '__main__':
     Path(loss_folder).mkdir(exist_ok=True)
 
 
-    train_loader = DataLoader(train_data, batch_size=4, shuffle=True, pin_memory=True, num_workers=4, collate_fn=collate_fn)
-    test_loader = DataLoader(test_data, batch_size=8, shuffle=False, pin_memory=True, num_workers=8, collate_fn=collate_fn)
+    train_loader = DataLoader(train_data, batch_size=10, shuffle=True, pin_memory=True, num_workers=10, collate_fn=collate_fn)
+    test_loader = DataLoader(test_data, batch_size=6, shuffle=False, pin_memory=True, num_workers=6, collate_fn=collate_fn)
 
     if torch.cuda.is_available:
         device = torch.device("cuda")
@@ -85,7 +85,8 @@ if __name__ == '__main__':
     model = ChestRCNN('../resnet/models/resnext101_32x8d_epoch_35.pt')
     model.to(device)
 
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, nesterov=True)
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.SGD(params, lr=0.001, momentum=0.9, nesterov=True)
 
     scaler = torch.cuda.amp.GradScaler()
 
@@ -123,7 +124,7 @@ if __name__ == '__main__':
         print(f"Training model on epoch {epoch} using lr={scheduler.get_last_lr()}")
 
         for batch_i, (imgs, targets) in enumerate(tqdm(train_loader, desc="Training")):
-        #for (imgs, targets) in itertools.islice(tqdm(train_loader, desc="Training"), 50):
+        #for (imgs, targets) in itertools.islice(tqdm(train_loader, desc="Training"), 10):
 
             imgs = imgs.to(device)
             targets = [{k: v.to(device).requires_grad_(False) for k, v in t.items()} for t in targets]

src/rcnn/utils.py
@@ -19,7 +19,6 @@ def evaluate(model, data_loader, device):
     n_threads = torch.get_num_threads()
     # FIXME remove this and make paste_masks_in_image run on the GPU
     torch.set_num_threads(1)
-    cpu_device = torch.device("cpu")
     model.eval()
     metric_logger = MetricLogger(delimiter="  ")
     header = 'Test:'
@@ -34,9 +33,17 @@ def evaluate(model, data_loader, device):
             torch.cuda.synchronize()
         model_time = time.time()
         outputs = model(images)
-
-        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+        #torchvision.ops.batched_nms(outputs['boxes'], outputs['scores'], )
+        #outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
         model_time = time.time() - model_time
+
+        outputs = [{k: v.to(torch.device("cpu")) for k, v in t.items()} for t in outputs]
+
+        #print("Before")
+        #print(outputs)
+        scale_boxes(outputs, targets)
+        #print("After")
+        #print(outputs)
 
         res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
         evaluator_time = time.time()
@@ -54,6 +61,20 @@ def evaluate(model, data_loader, device):
     coco_evaluator.summarize()
     torch.set_num_threads(n_threads)
     return coco_evaluator
+
+def scale_boxes(outputs, targets):
+    for target, output in zip(targets, outputs):
+        size = target['size'].detach().cpu().numpy()
+        width_factor = torch.tensor(size[0] / 1024, dtype=torch.float32)
+        height_factor = torch.tensor(size[1] / 1024, dtype=torch.float32)
+        for box in output['boxes']:
+            box[0] = box[0] * width_factor
+            box[2] = box[2] * width_factor
+            box[1] = box[1] * height_factor
+            box[3] = box[3] * height_factor
+
+
+
 
 class SmoothedValue(object):
     """Track a series of values and provide access to smoothed values over a
1429f27edabcfa920332b5267d509c4afedf6e1c
JulianSe26 <45558861+JulianSe26@users.noreply.github.com>
2021-09-26 14:32:48 +0200

    update take 2

--- src/yolo/train.py
+++ src/yolo/train.py
@@ -139,34 +139,24 @@ if __name__ == '__main__':
     elif not PRETRAINING:
         state_dict = torch.load(BEST_PRETRAINED_MODEL_CHEKPOINT, map_location=device)
         model.load_state_dict(state_dict, strict=False)  # load
-   
 
-    # Freeze
-    freeze = []  # parameter names to freeze (full or partial)
-    for k, v in model.named_parameters():
-        v.requires_grad = True  # train all layers
-        if any(x in k for x in freeze):
-            print('freezing %s' % k)
-            v.requires_grad = False
-    
 
-    pg0, pg1, pg2 = [], [], []  # optimizer parameter groups
+    # optimizer
+    accumulate = max(round(64 / BATCH_SIZE), 1)  # accumulate loss based on nominal batchsize of 64
+    HYPER_PARAMETERS["weight_decay"] *= BATCH_SIZE * accumulate / 64  # scale decay based on nominal batchsize of 64
+
+    pg0, pg1, pg2 = [], [], []  # parameter groups
     for k, v in model.named_modules():
         if hasattr(v, 'bias') and isinstance(v.bias, nn.Parameter):
-            pg2.append(v.bias)  # biases
+            pg2.append(v.bias)  # bias
         if isinstance(v, nn.BatchNorm2d):
-            pg0.append(v.weight)  # no decay
+            pg0.append(v.weight)  # no decay on batchnorm
         elif hasattr(v, 'weight') and isinstance(v.weight, nn.Parameter):
             pg1.append(v.weight)  # apply decay
 
-
-    nbs = 64  # nominal batch size
-    accumulate = max(round(nbs / BATCH_SIZE), 1)  # accumulate loss before optimizing
-    HYPER_PARAMETERS["weight_decay"] *= BATCH_SIZE * accumulate / nbs  # scale weight_decay
-
     optimizer = optim.SGD(pg0, lr=HYPER_PARAMETERS["lr0"], momentum=HYPER_PARAMETERS["momentum"], nesterov=True)
-    optimizer.add_param_group({'params': pg1, 'weight_decay': HYPER_PARAMETERS["weight_decay"]})  # add pg1 with weight_decay
-    optimizer.add_param_group({'params': pg2})  # add pg2 (biases)
+    optimizer.add_param_group({'params': pg1, 'weight_decay': HYPER_PARAMETERS["weight_decay"]}) 
+    optimizer.add_param_group({'params': pg2}) 
     del pg0, pg1, pg2
 
 
@@ -176,31 +166,31 @@ if __name__ == '__main__':
 
 
     # Image sizes
-    gs = max(int(model.stride.max()), 32) 
-    nl = model.model[-1].nl  # number of detection layers (used for scaling hyp['obj'])
-    imgsz, imgsz_test = [check_img_size(x, gs) for x in [IMG_SIZE, IMG_SIZE]]  # verify imgsz are gs-multiples
+    ms = max(int(model.stride.max()), 32) 
+    number_detecion_layers = model.model[-1].nl  
+    imgsz, imgsz_test = [check_img_size(x, ms) for x in [IMG_SIZE, IMG_SIZE]] 
 
     # Load data
-    train_loader, train_dataset = create_dataloader(data_folder_train, imgsz, BATCH_SIZE, gs, True,
+    train_loader, train_dataset = create_dataloader(data_folder_train, imgsz, BATCH_SIZE, ms, True,
                                             hyp=HYPER_PARAMETERS, augment=True, cache=True, workers= NUMBER_DATALOADER_WORKERS,
                                             prefix=colorstr('train: '))
-    validation_loader, validation_dataset = create_dataloader(data_folder_validation, imgsz, BATCH_SIZE, gs, True,
+    validation_loader, validation_dataset = create_dataloader(data_folder_validation, imgsz, BATCH_SIZE, ms, True,
                                             hyp=HYPER_PARAMETERS, augment=False, cache=True, workers= NUMBER_DATALOADER_WORKERS, rect=True,
                                             pad=.5, prefix=colorstr('val: '))
 
     print("data loading done..")
 
 
-    mlc = np.concatenate(train_dataset.labels, 0)[:, 0].max()  # max label class
-    nb = len(train_loader)  # number of batches
+    batches_nr = len(train_loader)  # number of batches
 
 
-    # Model parameters
-    HYPER_PARAMETERS['box'] *= 3. / nl 
-    HYPER_PARAMETERS['cls'] *= NUMBER_OF_CLASSES / 80. * 3. / nl 
-    HYPER_PARAMETERS['obj'] *= (imgsz / IMG_SIZE) ** 2 * 3. / nl 
+    # Model parameters (adapt changes to final yolo architecture)
+    HYPER_PARAMETERS['box'] *= 3. / number_detecion_layers 
+    HYPER_PARAMETERS['cls'] *= NUMBER_OF_CLASSES / 80. * 3. / number_detecion_layers 
+    HYPER_PARAMETERS['obj'] *= (imgsz / IMG_SIZE) ** 2 * 3. / number_detecion_layers 
     HYPER_PARAMETERS['label_smoothing'] = 0.0
     print(f"using loss factors: {HYPER_PARAMETERS['box']}, {HYPER_PARAMETERS['cls']}, {HYPER_PARAMETERS['obj']}")
+    
     model.nc = NUMBER_OF_CLASSES 
     model.hyp = HYPER_PARAMETERS 
     model.gr = 1.0 
@@ -209,7 +199,7 @@ if __name__ == '__main__':
 
 
     # Start training
-    number_warmups = max(round(HYPER_PARAMETERS["warmup_epochs"] * nb), 1000)  # number of warmup iterations, max(3 epochs, 1k iterations)
+    number_warmups = max(round(HYPER_PARAMETERS["warmup_epochs"] * batches_nr), 1000)  
     scheduler.last_epoch = - 1  
     scaler = amp.GradScaler(enabled=True)
     compute_loss = ComputeLoss(model) 
@@ -219,39 +209,41 @@ if __name__ == '__main__':
                 f'Starting training for {EPOCHS} epochs...\n'
                 f'Doing Gradient accumulations: {accumulate}')
 
+
+    # structures for loss report            
     losses_per_epoch = []
     single_val_losses_per_epoch = []
     val_losses_per_epoch = []
     general_test_results = []
-
-    mloss = torch.zeros(4, device=device)  # mean losses
-    mloss_val = torch.zeros(4, device=device)
+    # mean losses
+    mean_loss = torch.zeros(4, device=device) 
+    mean_loss_val = torch.zeros(4, device=device)
 
     for epoch in range(EPOCHS):
+        # put model in train mode
         model.train()
         losses = []
         single_losses = []
         for i, (imgs, targets, paths, _) in enumerate(tqdm.tqdm(train_loader, desc=colorstr('train: '))):
 
-            ni = i + nb * epoch 
+            ni = i + batches_nr * epoch 
             imgs = imgs.to(device, non_blocking=True).float() / 255.0 # scale images
 
              # Warmup iterations
             if ni <= number_warmups:
                 xi = [0, number_warmups]  # x interp
-                accumulate = max(1, np.interp(ni, xi, [1, nbs / BATCH_SIZE]).round())
-                for j, x in enumerate(optimizer.param_groups):
-                    # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
-                    x['lr'] = np.interp(ni, xi, [HYPER_PARAMETERS["warmup_bias_lr"] if j == 2 else 0.0, x['initial_lr'] * lf(epoch)])
-                    if 'momentum' in x:
-                        x['momentum'] = np.interp(ni, xi, [HYPER_PARAMETERS["warmup_momentum"], HYPER_PARAMETERS["momentum"]])
+                accumulate = max(1, np.interp(ni, xi, [1, 64 / BATCH_SIZE]).round()) # nominal batchsize=64
+                for j, p in enumerate(optimizer.param_groups):
+                    p['lr'] = np.interp(ni, xi, [HYPER_PARAMETERS["warmup_bias_lr"] if j == 2 else 0.0, p['initial_lr'] * lf(epoch)])
+                    if 'momentum' in p:
+                        p['momentum'] = np.interp(ni, xi, [HYPER_PARAMETERS["warmup_momentum"], HYPER_PARAMETERS["momentum"]])
 
             # YOLO Multi-scale
             if MULTI_SCALE:
-                sz = random.randrange(imgsz * 0.5, imgsz * 1.5 + gs) // gs * gs  # size
+                sz = random.randrange(imgsz * 0.5, imgsz * 1.5 + ms) // ms * ms  # size
                 sf = sz / max(imgs.shape[2:])  # scale factor
                 if sf != 1:
-                    ns = [math.ceil(x * sf / gs) * gs for x in imgs.shape[2:]] 
+                    ns = [math.ceil(x * sf / ms) * ms for x in imgs.shape[2:]] 
                     imgs = F.interpolate(imgs, size=ns, mode='bilinear', align_corners=False)
 
             # Forward pass
@@ -259,19 +251,16 @@ if __name__ == '__main__':
                 pred = model(imgs)
                 loss, loss_items = compute_loss(pred, targets.to(device))
 
-            # Backprop
+            # Backprop using scaler
             scaler.scale(loss).backward()
 
-          
-
-            mloss = (mloss * i + loss_items) / (i + 1)  # update mean losses
+            mean_loss = (mean_loss * i + loss_items) / (i + 1)  # update mean losses
             single_losses.append((loss_items[:3].cpu()/ len(train_loader)).numpy()) # box, obj, cls losses
 
             #Report loss
             if i % LOSS_REPORT_FREQUENCY == 0:
-                print(f'\nmean loss: {mloss}')
+                print(f'\nmean loss: {mean_loss}')
               
-
           
             # Gradient accumulations
             if ni % accumulate == 0:
@@ -281,52 +270,47 @@ if __name__ == '__main__':
 
         if epoch % SCHEDULER_REDUCE_FREQUENCY  == 0:
              scheduler.step()
-        losses_per_epoch.append(mloss.cpu().numpy())
+        losses_per_epoch.append(mean_loss.cpu().numpy())
         print(losses_per_epoch, type(losses_per_epoch))
 
         if epoch % VALDIATION_FREQUENCY == 0:
-            model.eval()
 
+            model.eval()
             iouv = torch.linspace(0.5, 0.55, 1).to(device)  # iou vector mAP@0.5:0.95
             niou = iouv.numel()
-
-            seen = 0 
-            p, r, f1, mp, mr, map50, map, t0, t1 = 0., 0., 0., 0., 0., 0., 0., 0., 0.
             loss = torch.zeros(3, device=device)
-            jdict, stats, ap, ap_class = [], [], [], []
+            image_statistics, ap, ap_class = [], [], []
             val_losses_items = []
             for j, (imgs, targets, paths, shapes) in enumerate(tqdm.tqdm(validation_loader)):
                 imgs = imgs.to(device, non_blocking=True)
                 imgs = imgs.float()
                 imgs /= 255.0  
                 targets = targets.to(device)
-                nb, _, height, width = imgs.shape  
+                number_batches, _, height, width = imgs.shape  
 
                 with torch.inference_mode(), torch.cuda.amp.autocast():
                     out, train_out = model(imgs, augment=False) 
                     
                     gloss, loss_items = compute_loss([x.float() for x in train_out], targets)  
-                    loss += loss_items[:3] # box, obj, cls
+                    loss += loss_items[:3] # box, objectness and class loss
 
 
-                mloss_val = (mloss_val * j + loss_items) / (j + 1)  # update mean losses
+                mean_loss_val = (mean_loss_val * j + loss_items) / (j + 1)
                 val_losses_items.append((loss.cpu() / len(validation_loader)).numpy())
 
-                # Run NMS
-                targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # pixels
-                lb =  []
-                out = non_max_suppression(out, HYPER_PARAMETERS["conf_threshold"], HYPER_PARAMETERS["iou_threshold"], labels=lb, multi_label=True, agnostic=True)
+                # Run non max supression
+                targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)
+                out = non_max_suppression(out, HYPER_PARAMETERS["conf_threshold"], HYPER_PARAMETERS["iou_threshold"], multi_label=True, agnostic=True)
 
-                # Statistics per image
+            
                 for si, pred in enumerate(out):
                     labels = targets[targets[:, 0] == si, 1:]
                     nl = len(labels)
                     tcls = labels[:, 0].tolist() if nl else []  # target class
-                    seen += 1
 
                     if len(pred) == 0:
                         if nl:
-                            stats.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
+                            image_statistics.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
                         continue
 
                     # Scale predictions back to original size
@@ -340,7 +324,7 @@ if __name__ == '__main__':
                         detected = []
                         tcls_tensor = labels[:, 0]
 
-                        # scale coords and further evaluation needs xyxy format
+                        # scale coords: further evaluation needs xyxy format
                         tbox = xywh2xyxy(labels[:, 1:5])
                         scale_coords(imgs[si].shape[1:], tbox, shapes[si][0], shapes[si][1])
                     
@@ -351,7 +335,7 @@ if __name__ == '__main__':
 
                             # Search for detections
                             if pi.shape[0]:
-                                ious, i = box_iou(predn[pi, :4], tbox[ti]).max(1)  # get the best ious for the predictions
+                                ious, i = box_iou(predn[pi, :4], tbox[ti]).max(1) 
 
                                 detected_set = set()
                                 for j in (ious > iouv[0]).nonzero(as_tuple=False):
@@ -363,18 +347,19 @@ if __name__ == '__main__':
                                         if len(detected) == nl: 
                                             break
 
-                    stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))
+                    image_statistics.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))
 
 
-            val_losses_per_epoch.append(mloss_val.cpu().numpy())
+            val_losses_per_epoch.append(mean_loss_val.cpu().numpy())
             single_val_losses_per_epoch.append(np.mean(val_losses_items, axis=1))
-            print(f"Validation Loss {mloss_val}")
-            stats = [np.concatenate(x, 0) for x in zip(*stats)] 
-            if len(stats) and stats[0].any():
-                p, r, ap, f1, ap_class = ap_per_class(*stats)
+            print(f"Validation Loss {mean_loss_val}")
+
+            stimage_statistics = [np.concatenate(x, 0) for x in zip(*image_statistics)] 
+            if len(image_statistics) and image_statistics[0].any():
+                p, r, ap, f1, ap_class = ap_per_class(*image_statistics)
                 ap50, ap = ap[:, 0], ap.mean(1) 
                 mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
-                nt = np.bincount(stats[3].astype(np.int64), minlength=NUMBER_OF_CLASSES)
+                nt = np.bincount(image_statistics[3].astype(np.int64), minlength=NUMBER_OF_CLASSES)
                 
                 general_test_results.append({
                 "precision": p,
@@ -393,8 +378,6 @@ if __name__ == '__main__':
             else:
                 nt = torch.zeros(1)
 
-            pf = '%20s' + '%12i' * 2 + '%12.3g' * 4 
-            print(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
 
         # Save losses, models and evaluation results
         if epoch % SAVE_FREQUENCY == 0:
